faf64070c8372d16379fbc6fa7e111d60ce992da
Nav Singh <navsingh191@gmail.com>
2021-08-05 22:26:34 -0400

    RichContentReceiver integrated with EditText. COIL library used to load the image from uri. App will handle the content if it has uri and rest of the content will be processed by the platform

--- app/src/main/java/com/example/splashscreenapisample/MainActivity.kt
+++ app/src/main/java/com/example/splashscreenapisample/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.splashscreenapisample
 
 import android.animation.ObjectAnimator
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.ViewTreeObserver
+import android.widget.ImageView
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.animation.doOnEnd
+import androidx.core.view.ContentInfoCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
+import coil.load
+import com.example.splashscreenapisample.contentreceivers.RichContentReceiver
 
 /**
  * @author Nav Singh
@@ -19,8 +28,61 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         content = findViewById(android.R.id.content)
+        // get reference to EditText
+        val appCompatEditText = findViewById<AppCompatEditText>(R.id.my_input)
+        // It's used to process the content, onReceive will trigger this
+        val contentResolve: (uri: Uri, source: Int) -> Unit = { uri, source ->
+            val contentResolver = this.contentResolver
+            val mimeType = contentResolver.getType(uri)
+            Log.d(TAG, "contentResolve: $mimeType Source is $source")
+            // MimeTypeMap.getSingleton().getMimeTypeFromExtension()
+            // check the source content
+            when (source) {
+                ContentInfoCompat.SOURCE_APP -> {
+                    Log.d(TAG, "contentResolve: Source ${ContentInfoCompat.SOURCE_APP}")
+                }
+                ContentInfoCompat.SOURCE_CLIPBOARD -> {
+                    Log.d(TAG, "contentResolve: Source ${ContentInfoCompat.SOURCE_CLIPBOARD}")
+                }
+                ContentInfoCompat.SOURCE_DRAG_AND_DROP -> {
+                    Log.d(TAG, "contentResolve: Source ${ContentInfoCompat.SOURCE_DRAG_AND_DROP}")
+                }
+                ContentInfoCompat.SOURCE_INPUT_METHOD -> {
+                    Log.d(TAG, "contentResolve: Source ${ContentInfoCompat.SOURCE_INPUT_METHOD}")
+                }
+            }
+
+            when {
+                mimeType?.contains("image/") == true -> {
+                    // process image
+                    Log.d(TAG, "onCreate: LOAD_IMAGE")
+                    val imageView = findViewById<ImageView>(R.id.image_holder)
+                    imageView.apply {
+                        isVisible = true
+                        load(uri)
+                    }
+                }
+                mimeType?.contains("video/") == true -> {
+                    // process video
+                    val videoView = findViewById<VideoView>(R.id.video_holder)
+                    videoView.apply {
+                        isVisible = true
+                        this.setVideoURI(uri)
+                    }
+                }
+            }
+        }
+        // add [OnReceiveContentListener] to appCompatEditText
+        ViewCompat.setOnReceiveContentListener(
+            appCompatEditText,
+            RichContentReceiver.MIME_TYPES,
+            RichContentReceiver(contentResolve)
+        )
+        // splash screen related code 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             Log.d("MainActivity", "onCreate: I AM RUNNING ON API 12 or higher")
             content.viewTreeObserver.addOnPreDrawListener(object :
@@ -55,5 +117,9 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
     }
 }
---
+++ app/src/main/java/com/example/splashscreenapisample/contentreceivers/RichContentReceiver.kt
@@ -0,0 +1,39 @@
+package com.example.splashscreenapisample.contentreceivers
+
+import android.content.ClipData
+import android.net.Uri
+import android.util.Log
+import android.view.View
+import androidx.core.view.ContentInfoCompat
+import androidx.core.view.OnReceiveContentListener
+
+/**
+ * @author Nav Singh
+ */
+class RichContentReceiver(val contentResolver: (uri: Uri, source: Int) -> Unit) :
+    OnReceiveContentListener {
+
+    companion object {
+        val TAG: String = RichContentReceiver::class.java.simpleName
+        val MIME_TYPES = arrayOf("image/*", "video/*", "text/*")
+    }
+
+    override fun onReceiveContent(view: View, payload: ContentInfoCompat): ContentInfoCompat? {
+        Log.d(TAG, "onReceiveContent: ${payload.clip.description}")
+        val pair = payload.partition {
+            it.uri != null
+        }
+        // image or video uri
+        if (pair.first != null) {
+            val clip: ClipData = pair.first.clip
+            for (i in 0 until clip.itemCount) {
+                val uri = clip.getItemAt(i).uri
+                Log.e(TAG, "onReceiveContent: $uri")
+                // process the uri
+                contentResolver(uri, pair.first.source)
+            }
+        }
+        // unhandled content system will process accordingly
+        return pair.second
+    }
+}

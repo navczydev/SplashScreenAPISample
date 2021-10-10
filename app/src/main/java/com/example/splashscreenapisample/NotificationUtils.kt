bfcd06eb4a40bb8c7e7900fc99199f8b5fe74426
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2021-10-09 21:28:18 -0400

    Notification BroadcastReceiver and util file added to send notification

---
+++ app/src/main/java/com/example/splashscreenapisample/NotificationUtils.kt
@@ -0,0 +1,49 @@
+package com.example.splashscreenapisample
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+/**
+ * @author Nav Singh
+ */
+private const val NOTIFICATION_CHANNEL_ID = BuildConfig.APPLICATION_ID + ".channel"
+
+fun sendNotification(context: Context) {
+    val notificationManager = context
+        .getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    // We need to create a NotificationChannel associated with our CHANNEL_ID before sending a notification.
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
+        && notificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID) == null
+    ) {
+        val name = context.getString(R.string.app_name)
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            name,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    val broadcastIntent = Intent(context.applicationContext, NotificationReceiver::class.java)
+    val actionIntent = PendingIntent.getBroadcast(
+        context.applicationContext,
+        0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+    )
+    val notification = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+        .setSmallIcon(R.mipmap.ic_launcher)
+        .setContentTitle("Android12")
+        .setContentText("Notification trampoline restrictions")
+        .addAction(R.mipmap.ic_launcher, "Open activity from receiver", actionIntent)
+        .setAutoCancel(true)
+        .build()
+
+    notificationManager.notify(getUniqueId(), notification)
+}
+
+private fun getUniqueId() = ((System.currentTimeMillis() % 10000).toInt())

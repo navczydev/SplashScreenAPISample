9209189bb5b069e7697a04999c0cd3176084039c
CzyDveloper <8358882+navczydev@users.noreply.github.com>
2021-10-09 21:26:46 -0400

    Activity added to demo the notification trampoline restriction

---
+++ app/src/main/java/com/example/splashscreenapisample/NotificationTrampolineActivity.kt
@@ -0,0 +1,14 @@
+package com.example.splashscreenapisample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * @author Nav Singh
+ */
+class NotificationTrampolineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
